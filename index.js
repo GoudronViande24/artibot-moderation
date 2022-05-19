89dccd641af3c898cafbdf35086f04457174d79c
Thomas Fournier <thomas@artivain.com>
2022-05-19 19:10:23 -0400

    Update for artibot v3

--- index.js
+++ index.js
@@ -1,39 +1,105 @@
-// Manifest file for moderation module
+import { SlashCommandBuilder } from "@discordjs/builders";
+import Artibot, { Button, Module, SlashCommand } from "artibot";
+import Localizer from "artibot-localizer";
 
-module.exports = {
-	manifest: {
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from 'module';
 
-		manifestVersion: 1,
-		moduleVersion: "1.2.0",
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const { version } = require('./package.json');
+
+import muteSlashCommand from "./mute.js";
+import unmuteSlashCommand from "./unmute.js";
+import purgeSlashCommand from "./purge.js";
+import purgeButton from "./purgebutton.js";
+
+/**
+ * Moderation module for Artibot
+ * @author GoudronViande24
+ * @license MIT
+ * @param {Artibot} artibot
+ * @returns {Module}
+ */
+export default ({ config: { lang } }) => {
+	localizer.setLocale(lang);
+
+	return new Module({
+		id: "moderation",
 		name: "Moderation",
-		supportedLocales: [
+		version,
+		langs: [
 			"en",
 			"fr"
 		],
+		repo: "GoudronViande24/artibot-moderation",
 		parts: [
-			{
+			new SlashCommand({
 				id: "mute",
-				type: "slashcommand",
-				path: "mute.js"
-			},
+				data: new SlashCommandBuilder()
+					.setName("mute")
+					.setDescription(localizer._("Mute a user."))
+					.addUserOption(option =>
+						option
+							.setName("user")
+							.setDescription(localizer._("The user to mute."))
+							.setRequired(true)
+					)
+					.addStringOption(option =>
+						option
+							.setName("duration")
+							.setDescription(localizer._("How much time the user must stay muted. Examples: '5m', '1h'."))
+							.setRequired(true)
+					)
+					.addStringOption(option =>
+						option
+							.setName("reason")
+							.setDescription(localizer._("The reason why the user gets muted."))
+					),
+				mainFunction: muteSlashCommand
+			}),
 
-			{
+			new SlashCommand({
 				id: "unmute",
-				type: "slashcommand",
-				path: "unmute.js"
-			},
+				data: new SlashCommandBuilder()
+					.setName("unmute")
+					.setDescription(localizer._("Unmutes a user."))
+					.addUserOption(option =>
+						option
+							.setName("user")
+							.setDescription(localizer._("The user to unmute."))
+							.setRequired(true)
+					),
+				mainFunction: unmuteSlashCommand
+			}),
 
-			{
+			new SlashCommand({
 				id: "purge",
-				type: "slashcommand",
-				path: "purge.js"
-			},
+				data: new SlashCommandBuilder()
+					.setName("purge")
+					.setDescription(localizer._("Mass delete messages."))
+					.addIntegerOption(option =>
+						option
+							.setName("amount")
+							.setDescription(localizer._("How much messages to delete?"))
+							.setRequired(true)
+							.setMaxValue(100)
+							.setMinValue(0)
+					),
+				mainFunction: purgeSlashCommand
+			}),
 
-			{
+			new Button({
 				id: "purge-*",
-				type: "button",
-				path: "purgebutton.js"
-			}
+				mainFunction: purgeButton
+			})
 		]
-	}
-};
+	});
+}
+
+export const localizer = new Localizer({
+	filePath: path.join(__dirname, "locales.json")
+});
